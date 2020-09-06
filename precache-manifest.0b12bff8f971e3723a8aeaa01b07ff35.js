68237a6efdb5560e2a14814b61a31193efeb545c
Bartlomiej95 <panfilbartlomiej@gmail.com>
2020-09-06 17:18:44 +0200

    Updates

---
+++ precache-manifest.0b12bff8f971e3723a8aeaa01b07ff35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972b3a4ba2b4498caff9bedf5f55aed2",
+    "url": "/HomePantry/index.html"
+  },
+  {
+    "revision": "e4d2a5290fbe1b2a9b5f",
+    "url": "/HomePantry/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c99757c34c873340418d",
+    "url": "/HomePantry/static/js/2.8a24248e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HomePantry/static/js/2.8a24248e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d2a5290fbe1b2a9b5f",
+    "url": "/HomePantry/static/js/main.3cda02bf.chunk.js"
+  },
+  {
+    "revision": "7ababfa15378dffc176b",
+    "url": "/HomePantry/static/js/runtime-main.43f62610.js"
+  }
+]);
